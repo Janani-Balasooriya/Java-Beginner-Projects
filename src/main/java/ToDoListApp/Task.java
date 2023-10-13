445647cffb7dc81fe4ea1464d6b285c9c0ae970c
Janani-Balasooriya <jananidev7@gmail.com>
2023-10-13 23:35:28 +0530

    Add To-Do List App code

---
+++ src/main/java/ToDoListApp/Task.java
@@ -0,0 +1,23 @@
+package ToDoListApp;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
