091735dacc27e1bcd0350ff9885c9b330b505689
Bashiru <mohammedbashiruissahaku0@gmail.com>
2024-08-26 22:11:30 +0000

    Update Calculator.java
    documented part of code for better understanding 
    added scanner.close() to avoid data leak

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -2,24 +2,35 @@ package Calculator;
 
 import java.util.Scanner;
 
-public class Calculator {
+public class Calculator
+	{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+		{
 		// TODO Auto-generated method stub
+
+		// result will be in float form 
 		float res = 0;
+
+		
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Enter the 1st number :");
+		System.out.println("Enter the 1st number : ");
 		float num1 = userInput.nextFloat();
 		
 		System.out.println("Enter the 2nd number :");
 		float num2 = userInput.nextFloat();
-		
+
+		// print basic mathematical operators
 		System.out.println("Enter the operation :");
 		System.out.println("1. +");
 		System.out.println("2. -");
 		System.out.println("3. *");
 		System.out.println("4. /");
+
+		// using the scanner class to take input which will only be in int form
 		int optr = userInput.nextInt();
+
+		//using control statement to print the operator statements and also handle some errors statements 
 		if(optr>4)
 		{
 			System.out.println("Error");
@@ -45,6 +56,9 @@ public class Calculator {
 		}
 		System.out.println("The result is " + res);
 		}
+
+		// close scanner to avoid data leak
+		Scanner.close();
 	}
 
 }
